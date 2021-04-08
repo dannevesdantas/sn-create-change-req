830545c5eb6c46dfeea6ece9f8d598587a3f150e
Danilo Dantas <5115895+dannevesdantas@users.noreply.github.com>
2021-04-08 01:02:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const axios = require('axios');
 
 try {
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
+    const instanceUrl = core.getInput('instance-url');
+    console.log('Instance Url is ${nameToGreet}!');
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    console.log('The event payload: ${payload}');
 } catch (error) {
     core.setFailed(error.message);
 }
