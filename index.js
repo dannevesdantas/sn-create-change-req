8171c7f2724ec6df1c4f41fe596fecde2b08d661
Danilo Dantas <5115895+dannevesdantas@users.noreply.github.com>
2021-04-08 02:01:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const axios = require('axios');
 
 try {
@@ -9,7 +8,7 @@ try {
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    //const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log('The event payload: ${payload}');
 } catch (error) {
     core.setFailed(error.message);
