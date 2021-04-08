f7ad0d6b565fc5e34bde83953254d6c285bdeec2
Danilo Dantas <5115895+dannevesdantas@users.noreply.github.com>
2021-04-08 02:14:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,32 @@ try {
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     //const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    //console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
+
+// teste Danilo
+
+var config = {
+    method: 'post',
+    url: 'https://dev82459.service-now.com/api/sn_chg_rest/change',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Basic YWRtaW46ajZMdXNxbFUzVUNF',
+        'Content-Type': 'application/json'
+    },
+    data: JSON.stringify({
+        "description": "Deploy api-marketing B2B"
+    })
+};
+
+axios(config)
+    .then(function (response) {
+        //console.log(JSON.stringify(response.data));
+        console.log(response.data.result.number.value);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
