34180902973f276986d5ba6fa6ab205742d1e2a1
Danilo Dantas <5115895+dannevesdantas@users.noreply.github.com>
2021-04-15 09:30:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const axios = require('axios');
 var server;
 var username;
 var password;
+var fields;
+var additionalFields;
 
 try {
-    server = core.getInput('server');
+    server = core.getInput('server', { required: true });
     username = core.getInput('username');
     password = core.getInput('password');
+
+    if (core.getInput('short_description')) { fields["short_description"] = core.getInput('short_description', { required: true }); }
+    if (core.getInput('description')) { fields["description"] = core.getInput('description'); }
+    if (core.getInput('configuration_item')) { fields["configuration_item"] = core.getInput('configuration_item'); }
+    if (core.getInput('assignment_group')) { fields["assignment_group"] = core.getInput('assignment_group'); }
+    if (core.getInput('assigned_to')) { fields["assigned_to"] = core.getInput('assigned_to'); }
+    if (core.getInput('state')) { fields["state"] = core.getInput('state'); }
+    if (core.getInput('priority')) { fields["priority"] = core.getInput('priority'); }
+    if (core.getInput('comments')) { fields["comments"] = core.getInput('comments'); }
+
+    if (core.getInput('additional_fields')) {
+        additionalFields = core.getInput('additional_fields');
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
@@ -26,23 +41,7 @@ axios({
         'Accept': 'application/json',
         'Content-Type': 'application/json'
     },
-    data: JSON.stringify({
-        "short_description":"Uma nova versao (1.2.0.25) das aplicacoes:\nB2B.Preco.Host.WebApi;B2B.Preco.Host.Console\nEsta disponivel para release",
-        "assignment_group":"f0707de1db206780e1b032ffaa9619ed",
-        "cmdb_ci":"a7baff87dbf7334830404410ba961924",
-        "backout_plan":"Criar nova release de rollback",
-        "description":"Uma nova versao (1.2.0.25) das aplicacoes:\nB2B.Preco.Host.WebApi;B2B.Preco.Host.Console\nEsta disponivel para release",
-        "u_change_trigger":"project",
-        "type":"continual",
-        "x_xlbv_xl_release_identifier":"Applications/Folder9fbbec040721402eaf0acc000e0eae5b/Releasea88076a891b9450cae93fe89d2a38814",
-        "x_xlbv_xl_release_state":"IN_PROGRESS",
-        "u_classification":"7cc8727adb88e34070039d40ba9619a7",
-        "test_plan":"Testes automatizados + aprovacao do time de QA",
-        "change_plan":"Comunicar time B2B - Preço",
-        "justification":"Time<span class=\"xl-ws\"> </span>B2B - Preço<span class=\"xl-ws\"> </span>tem uma nova versao para release",
-        "assigned_to":"xl.integration",
-        "start_date":"2019-10-20 23:22:25"
-    })
+    data: JSON.stringify(fields)
 }).then(function (response) {
     //console.log(JSON.stringify(response.data));
     var number = response.data.result.number;
